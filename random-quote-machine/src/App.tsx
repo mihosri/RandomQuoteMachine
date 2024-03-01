28e491157e8e58466c5706958d6a5dd2e7dd9ddb
mihosri <113211399+mihosri@users.noreply.github.com>
2024-03-01 12:37:11 -0700

    Added quote icons around the quote

--- random-quote-machine/src/App.tsx
+++ random-quote-machine/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import quotes from "./assets/quotes.json"
+import { FaTwitter, FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 import './App.css'
 
 interface Quote{
@@ -16,16 +17,14 @@ function App() {
 
   return (
     <div>
-     <div id="quote-box">
-      <div className="quote-content">
-        <h2 id="text">
-          {quote.quote}
-        </h2>
-        <h4 id="author">
-          -{quote.author}
-        </h4>
+      <div id="quote-box">
+        <div className="quote-content">
+          <FaQuoteLeft size="30" style={{ marginRight: '10px' }} />
+          <h2 id="text">{quote.quote}</h2>
+          <FaQuoteRight size="30" style={{ marginLeft: '10px' }} />
+          <h4 id="author">-{quote.author}</h4>
+        </div>
       </div>
-     </div> 
     </div>
   )
 }
